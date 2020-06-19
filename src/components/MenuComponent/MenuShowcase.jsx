df5337064119f40d645d278e827601d2a51ae910
jeffascript <ofoaro.jeff@gmail.com>
2020-06-19 15:38:36 +0200

    showcase fix

--- src/components/MenuComponent/MenuShowcase.jsx
+++ src/components/MenuComponent/MenuShowcase.jsx
@@ -1,88 +1,62 @@
-import React from 'react';
-import "./Menu.css"
-import 'antd/dist/antd.css';
-import { menuPlan } from "./MenuObject"
+import React from "react";
+import "./Menu.css";
+import "antd/dist/antd.css";
+import { menuPlan } from "./MenuObject";
 
+import { Tabs } from "antd";
+import { Container, Row, Col } from "reactstrap";
+const { TabPane } = Tabs;
+// const { Option } = Select;
 
-import { Tabs, Select, Space } from 'antd'
-import { Container, Row, Col } from 'reactstrap';
-const { TabPane } = Tabs
-const { Option } = Select;
+export const MealItem = ({ menuItem }) => {
+  let result;
 
+  menuItem === "All" || "" || undefined
+    ? (result = menuPlan.filter(
+        ({ menu }) => !menu.some((tag) => tag === menuItem)
+      ))
+    : (result = menuPlan.filter(({ menu }) =>
+        menu.some((tag) => tag === menuItem)
+      ));
 
+  // console.log(result);
+  return result.map(({ id, recipe, price, image, dishname }) => (
+    <>
+      <div index={id} className="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+        <Row>
+          <div className="col">
+            <div>
+              <img className=" small-Img" alt="cupcake" src={image} />
+            </div>
+          </div>
+          <Col>
+            <div>
+              <h4> {dishname} </h4>
 
+              <div>
+                <p className="text-left">
+                  {recipe.map((oneRecipe) => (
+                    <>
+                      {" "}
+                      <span>{oneRecipe} </span>{" "}
+                    </>
+                  ))}
+                </p>
+              </div>
+            </div>
 
- export const MealItem = ( { menuItem }) =>{
-
-    let result;
-
-        menuItem ==="All" || "" || undefined
-        ? result = menuPlan.filter(({ menu }) => !menu.some(tag => tag===menuItem))
-        : result = menuPlan.filter(({ menu }) => menu.some(tag => tag===menuItem))
-
-        console.log(result)
-        return( 
-
-
-    result.map(({ id, recipe, price, image, dishname }) =>
-
-       
-<>
-       
-            
-       <div index={id} className="col-lg-4 col-md-4 col-sm-4 col-xs-6">
-             <Row>
-               <div className="col">
-                 <div>
-                   <img className=" small-Img" alt="cupcake" src={image} />
-                 </div>
-               </div>
-               <Col>
-                 <div>
-                   <h4> { dishname} </h4>
-                  
-                  
-                      
-                     <div>
-                            <p className="text-left">
-                                   { 
-                                   
-                                   recipe.map( oneRecipe =>( <>  <span>{ oneRecipe} </span> </>))
-                               }
-                            </p>
-                     </div>
-                       
-                  
-                 </div>
-                
-               <h4 className="mb-5"> { price} </h4>
-              
-               </Col>
-               
-             </Row>
-
-             
-           </div>
-           
-      </> 
-        )
-        
-        )
-
-        }
-
-
-
-
-
+            <h4 className="mb-5"> {price} </h4>
+          </Col>
+        </Row>
+      </div>
+    </>
+  ));
+};
 
 const MenuShowcase = ({ tabPosition, changeTabPosition }) => {
-
-    
-
-    return (
-      <div>
-        {/* <Space style={{ marginBottom: 16 }}>
+  return (
+    <div>
+      {/* <Space style={{ marginBottom: 16 }}>
           Tab position：
           <Select
             value={tabPosition}
@@ -95,8 +69,8 @@ const MenuShowcase = ({ tabPosition, changeTabPosition }) => {
             <Option value="right">right</Option>
           </Select>
         </Space> */}
-        <Tabs tabPosition={tabPosition}>
-          {/* {[...Array(6).keys()].map(i => (
+      <Tabs tabPosition={tabPosition}>
+        {/* {[...Array(6).keys()].map(i => (
   <TabPane tab={`Tab-${i}3453`} key={i} enabled={i === 6}>
    
    <div>Content of tab {i} </div>
@@ -105,10 +79,10 @@ const MenuShowcase = ({ tabPosition, changeTabPosition }) => {
   </TabPane>
 ))} */}
 
-          <TabPane tab="All" key="1">
-            <Container className="h-100 w-100">
-              <Row className="h-100 w-100 justify-content-between align-content-center">
-                {/* <div className="col-lg-4 col-md-5 col-sm-4 col-xs-6">
+        <TabPane tab="All" key="1">
+          <Container className="h-100 w-100">
+            <Row className="h-100 w-100 justify-content-between align-content-center">
+              {/* <div className="col-lg-4 col-md-5 col-sm-4 col-xs-6">
                   <Row>
                     <div className="col">
                       <div>
@@ -135,36 +109,31 @@ const MenuShowcase = ({ tabPosition, changeTabPosition }) => {
                   </Row>
                 </div> */}
 
+              <MealItem menuItem="All" />
+            </Row>
+          </Container>
+        </TabPane>
+        <TabPane tab="Breakfast" key="2">
+          <MealItem menuItem="Breakfast" />
+        </TabPane>
+        <TabPane tab="Lunch" key="3">
+          <MealItem menuItem="Lunch" />
+        </TabPane>
 
-<MealItem  menuItem="All"/>
+        <TabPane tab="Brunch" key="4">
+          <MealItem menuItem="Brunch" />
+        </TabPane>
+        <TabPane tab="Snack" key="5">
+          <MealItem menuItem="Snack" />
+        </TabPane>
 
-
-              </Row>
-            </Container>
-          </TabPane>
-          <TabPane tab="Breakfast" key="2">
-                <MealItem  menuItem="Breakfast"/>
-
-         
-          </TabPane>
-          <TabPane tab="Lunch" key="3">
-          <MealItem  menuItem="Lunch"/>
-          </TabPane>
-
-          <TabPane tab="Brunch" key="4">
-          <MealItem  menuItem="Brunch"/>
-          </TabPane>
-          <TabPane tab="Snack" key="5">
-          <MealItem  menuItem="Snack"/>
-          </TabPane>
-
-          <TabPane tab="Dinner" key="6">
-          <MealItem  menuItem="Dinner"/>
-          </TabPane>
-        </Tabs>
-        <hr/>
-      </div>
-    );
+        <TabPane tab="Dinner" key="6">
+          <MealItem menuItem="Dinner" />
+        </TabPane>
+      </Tabs>
+      <hr />
+    </div>
+  );
 };
 
 export default MenuShowcase;
