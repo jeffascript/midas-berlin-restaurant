5fd1899b0d3366b663b1d10dd2907c7045a99bc8
jeffascript <ofoaro.jeff@gmail.com>
2020-06-21 21:45:52 +0200

    ++

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { BsCreditCard } from "react-icons/bs";
@@ -15,8 +15,6 @@ const Information = () => {
 
   return (
     <>
-
-
       <Container style={{ padding: "7em 0" }}>
         <Row>
           <div className="col-12 mt-3 mb-4">
@@ -35,41 +33,33 @@ const Information = () => {
               <div data-aos="zoom-in-up">
                 <Row className="text-justify text-white justify-content-between align-content-center">
                   <div className="font-weight-bold "> Monday - Friday</div>
-                  <div >10:00 - 22:00 </div>
+                  <div>10:00 - 22:00 </div>
                 </Row>
                 <Row className="text-justify text-white justify-content-between align-content-center">
                   <div className="font-weight-bold "> Saturday - Sunday</div>
-                  <div >09:00 - 20:00 </div>
+                  <div>09:00 - 20:00 </div>
                 </Row>
               </div>
-
-                                
-
             </div>
           </Container>
           <div className="col-12 mt-3 mb-4">
             <Container>
               <div data-aos="fade-up">
                 <div className="text-center mx-auto sub-header d-flex  justify-content-around align-content-center mt-5">
-
-
-
-
-                
-
-
-
-<div>
-                    <BsCreditCard style={{fontSize:"12vmin"}}/>
+                  <div>
+                    <BsCreditCard style={{ fontSize: "12vmin" }} />
                     <div className="text-white">EC &amp; Mastercard</div>
-                </div>
-                                <div><IoMdCash style={{fontSize:"12vmin"}}/>
-                                <div className="text-white">Cash</div>
-                                </div>
+                  </div>
+                  <div>
+                    <IoMdCash style={{ fontSize: "12vmin" }} />
+                    <div className="text-white">Cash</div>
+                  </div>
 
-                               <div > <GiFoodTruck style={{fontSize:"12vmin"}}/>
-                               <div className="text-white">Home delivery</div>
-                               </div>
+                  <div>
+                    {" "}
+                    <GiFoodTruck style={{ fontSize: "12vmin" }} />
+                    <div className="text-white">Home delivery</div>
+                  </div>
                 </div>
               </div>
             </Container>
