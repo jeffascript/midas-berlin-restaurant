4624b58aabf7367f39f353c64ce4dded821ba79b
jeffascript <ofoaro.jeff@gmail.com>
2020-06-28 00:23:01 +0200

    ++

--- src/components/GalleryComponent/GalleryMasonry.jsx
+++ src/components/GalleryComponent/GalleryMasonry.jsx
@@ -3,29 +3,27 @@ import { Container, Row } from "reactstrap";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Gallery from "react-photo-gallery";
-import PhotoHelper from "./PhotoHelper"
+import PhotoHelper from "./PhotoHelper";
 import arrayMove from "array-move";
 import { SortableContainer, SortableElement } from "react-sortable-hoc";
-import { photos } from "./PhotoAlbumArray"
+import { photos } from "./PhotoAlbumArray";
 
-
-
-const SortablePhoto = SortableElement(item => <PhotoHelper {...item} />);
+const SortablePhoto = SortableElement((item) => <PhotoHelper {...item} />);
 
 const SortableGallery = SortableContainer(({ items }) => (
-  <Gallery photos={items} renderImage={props => <SortablePhoto {...props} />} />
-)); 
-
-
+  <Gallery
+    photos={items}
+    renderImage={(props) => <SortablePhoto {...props} />}
+  />
+));
 
 const GalleryMasonry = () => {
-    const [items, setItems] = useState(photos);
+  const [items, setItems] = useState(photos);
 
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-      setItems(arrayMove(items, oldIndex, newIndex));
-    };
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setItems(arrayMove(items, oldIndex, newIndex));
+  };
 
-    
   useEffect(() => {
     AOS.init({
       duration: 2000,
@@ -37,21 +35,27 @@ const GalleryMasonry = () => {
       <Row>
         <div className="col-12 mt-3 mb-4">
           <Container>
-            <div data-aos="fade-up">
-              <h1 className="text-center mx-auto sub-header">
-                Photo Gallery
-              </h1>
+            <div data-aos="fade-down">
+              <h1 className="text-center mx-auto sub-header">Photo Gallery</h1>
               <div className="header-divider"></div>
             </div>
           </Container>
         </div>
 
         <div className="col-12">
-        <div>
-    
-      <h3 className="text-center mx-auto ">Drag and drop photos to rearrange</h3>
-      <SortableGallery items={items} onSortEnd={onSortEnd} axis={"xy"} />
-    </div>
+          <div>
+            <h3 className="text-center mx-auto ">
+              Drag and drop photos to rearrange
+            </h3>
+            <div data-aos="fade-up">
+              {" "}
+              <SortableGallery
+                items={items}
+                onSortEnd={onSortEnd}
+                axis={"xy"}
+              />
+            </div>
+          </div>
         </div>
       </Row>
     </Container>
